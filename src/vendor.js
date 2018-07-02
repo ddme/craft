dcdf27c1ee181e97e1f8f57ceec6db01a9003d8a
lukeyouell <>
2018-07-02 14:12:57 +0100

    updated vendor

--- src/vendor.js
+++ src/vendor.js
@@ -1,25 +1,54 @@
-import cookieconsent from 'cookieconsent';
+import $ from 'jquery';
+window.$ = $;
 
-window.addEventListener('load', function() {
-  window.cookieconsent.initialise({
-    'palette': {
-      'popup': {
-        'background': '#2B333E',
-        'text': '#ffffff'
-      },
-      'button': {
-        'background': '#30A9DE',
-        'text': '#ffffff'
-      }
-    },
-    'theme': 'block',
-    'position': 'bottom-right',
-    'content': {
-      'dismiss': 'I understand'
-    }
-  })
+import cookies from 'js-cookie';
+
+window.cookies = cookies;
+
+var container = '#cookie_consent';
+var dismiss = '#cookie_dismiss';
+var consent = cookies.get('cookie_consent');
+
+if (consent == undefined) {
+  $(container).fadeIn();
+  $(dismiss).on('click', function() {
+    cookies.set('cookie_consent', 'active', { expires: 7 });
+    $(container).fadeOut();
+  });
+}
+
+import lazyload from 'lazyload';
+
+new lazyload();
+
+import parsley from 'parsleyjs';
+
+$('form').parsley({
+  errorClass: 'has-errors',
+  successClass: 'has-success',
+  classHandler: function (el) {
+    return el.$element.closest('.field-group');
+  },
+  errorsWrapper: '<div class="field-errors"></div>',
+  errorTemplate: '<div class="field-error"></div>',
 });
 
-import hyperform from 'hyperform';
+$('#js-burger').on('click', function () {
+  $('#js-navigation').toggleClass('hidden');
+});
 
-hyperform(window);
+$('input[type="checkbox"]').on('change', function() {
+  $(this).parent().toggleClass('checked');
+});
+
+$('input[type="radio"]').on('change', function() {
+  $('input[type="radio"]:not(:checked)').parent().removeClass('checked');
+  $('input[type="radio"]:checked').parent().addClass('checked');
+});
+
+import mask from 'jquery-mask-plugin';
+
+$('.input-date').mask('00/00/0000');
+$('.input-time').mask('00:00:00');
+$('.input-datetime').mask('00/00/0000 00:00:00');
+$('.input-phone').mask('00000 000000');
